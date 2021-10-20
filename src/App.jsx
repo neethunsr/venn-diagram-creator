a233cceba8dff0116bd98767d3cc20f107dd23e7
neethunsr <neethu2001nsr@gmail.com>
2021-10-20 13:58:59 +0530

    Implement Venn Diagram

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,13 @@
 import React from 'react';
 import Venn from './venn';
+import './venn.css'
 
 function App() {
 
   return (
     <div className="App">
-      <h1>Venn Diagram Creator</h1>
-      <Venn />
-      
-      {/* <VennDiagram
-      height={300}
-      width={300}
-      data={[
-        { key: ['A'], data: 12 },
-        { key: ['B'], data: 12 },
-        { key: ['A', 'B'], data: 2 }
-      ]}
-    /> */}
+      <h1>Venn Diagram Maker</h1>
+        <Venn />
     </div>
   );
 }
--- src/venn.jsx
+++ src/venn.jsx
@@ -1,44 +1,78 @@
 import React, {useState} from 'react';
-import { VennDiagram } from 'reaviz';
-import TextField from '@mui/material/TextField'
+import { VennArc, VennDiagram, VennSeries } from 'reaviz';
+import TextField from '@mui/material/TextField';
+import './venn.css';
+
 
 function Venn(){
+    const [valueA, setValueA] = useState();
+    const [valueB, setValueB] = useState();
+    const [valueAB, setValueAB] = useState();
 
+    const handleChangeA = (event) => {
+        setValueA(event.target.valueAsNumber);
+    };
+    const handleChangeB = (event) => {
+        setValueB(event.target.valueAsNumber);
+    };
+    const handleChangeAB = (event) => {
+        setValueAB(event.target.valueAsNumber);
+    };
     const values = [
-        {key: ['A'], data: 12},
-        {key: ['B'], data: 12},
-        {key: ['A', 'B'], data: 2}
+        {key: ['A'], data: valueA},
+        {key: ['B'], data: valueB},
+        {key: ['A', 'B'], data: valueAB}
     ];
+
     return(
     <div>
     <form>
         <TextField 
+            className="textfield"
             id="standard-basic" 
             label="Value of A" 
             variant="standard" 
             type="number"
+            margin="normal"
+            value={valueA}
+            onChange={handleChangeA}
         />
+        <br />
         <TextField 
             id="standard-basic" 
             label="Value of B" 
             variant="standard"
             type="number"
+            value={valueB}
+            margin="normal"
+            onChange={handleChangeB}
         />
+        <br />
         <TextField 
             id="standard-basic" 
             label="Value of A U B" 
             variant="standard" 
             type="number"
+            margin="normal"
+            value={valueAB}
+            onChange={handleChangeAB}
         />
-        <button
-            type="submit">
-            Submit
-        </button>
     </form>
     <VennDiagram
-        height={300}
-        width={300}
+        type='euler'
+        height={500}
+        width={400}
         data={values}
+        series={
+            <VennSeries
+                colorScheme='#035170'
+                arc={
+                    <VennArc
+                        stroke='#035170'
+                    />
+                }
+            />
+        }
     />
     </div>
     );
